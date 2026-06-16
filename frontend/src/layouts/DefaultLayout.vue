cc6d4e789e0b567060291cd32e2caf56789d962c
Aina Radanielina <ainradan@c1r2s12.42antananarivo.mg>
2026-06-16 13:13:57 +0300

    add frontend

---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="px-32 pt-10">
+    <header class="navbar bg-base-100 shadow-sm rounded-4xl px-10 border-b border-b-emerald-300">
+      <div class="navbar-start">
+        <p class=" text-xl font-bold text-primary">42Archivement</p>
+      </div>
+      <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal px-1">
+          <!-- <li><router-link to="/">Archivement</router-link></li>
+          <li><router-link to="/">staff</router-link></li> -->
+        </ul>
+      </div>
+      <div class="navbar-end space-x-10">
+        <p class="font-bold text-md">User</p>
+        <a class="btn btn-primary">Logout</a>
+      </div>
+    </header>
+
+    <main class="content">
+      <router-view />
+    </main>
+  </div>
+</template>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+</script>
+
+<template>
+    <DefaultLayout>
+        <p>home</p>
+    </DefaultLayout>
+</template>
